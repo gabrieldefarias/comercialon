ac15304985f579a8674fe2ed4b1dd8424c6fa82b
GABRIEL DE FARIAS OLIVEIRA <gabriel.foliveira21@senacsp.edu.br>
2021-06-15 11:51:09 -0300

    15/06/2021 - implementando classe cliente

---
+++ comercialon/classes/cliente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace comercialon.classes
+{
+    public class Cliente // classe publica // projeto de aula - ti89
+    { // declaração de atributo
+        public int Id {get; set;}
+        public string Nome {get; set;}
+        // métodos construtores
+
+
+        public Cliente()
+        {}
+        public Cliente(string nome)
+        {
+            Nome = nome;
+        }
+        public Cliente(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+        // métodos da classe
+        public void Inserir()
+        {}
+        public bool Alterar(int id)
+        {
+            return true;
+        }
+        public List<Cliente> ListarTodos()
+        {
+            List<Cliente> lista = new List<Cliente>();
+            // codigo de listar
+            return lista;
+        }
+        public void BuscarId(int id)
+        {
+
+        }
+    }
+}
