924c6eb7ba0977408f3fef6cb40eed026ac7dae6
GABRIEL DE FARIAS OLIVEIRA <gabriel.foliveira21@senacsp.edu.br>
2021-06-16 11:43:06 -0300

    16/06/2021 - usuario e produto

--- comercialon/classes/Cliente.cs
+++ comercialon/classes/Cliente.cs
@@ -3,31 +3,58 @@ using System.Collections.Generic;
 
 namespace comercialon.classes
 {
-    public class Cliente // classe publica // projeto de aula - ti89 - comercialon
+    public class Cliente // classe publica // projeto de aula - ti89
     { // declaração de propriedades
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Cpf { get; set; }
         public string Email{ get; set; }
         public string Telefone { get; set; }
+        public bool Ativo { get; set; }
+        public Endereco Endereco { get; set; }
+        //========================================================================================
 
-
-        public Cliente()
+        public Cliente() // gerando os construtores
         {
             Id = 0;
         }
 
+        public Cliente(string nome, string cpf, string email, string telefone, bool ativo = true, Endereco endereco = null)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            Email = email;
+            Telefone = telefone;
+            Ativo = ativo;
+            Endereco = endereco;
+        }
+
+        public Cliente(int id, string nome, string cpf, string email, string telefone, bool ativo = true, Endereco endereco = null)
+        {
+            Id = id;
+            Nome = nome;
+            Cpf = cpf;
+            Email = email;
+            Telefone = telefone;
+            Ativo = ativo;
+            Endereco = endereco;
+        }
+        //========================================================================================
+
         public void Inserir() // métodos da classe // inserir
         {}
+
         public bool Alterar(int id) // alterar
         {
             return true;
         }
+
         public List<Cliente> ListarTodos() // listar todos os clientes
         {
             List<Cliente> lista = new List<Cliente>(); // codigo de listar cliente
             return lista; // retornar a lista de cliente
         }
+
         public void BuscarId(int id) // buscar id
         {}
     }
